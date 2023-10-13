2bf1763d0e0a1ec6c719f179c173566ddf940e50
Don David <davidguzman39@gmail.com>
2023-10-12 23:52:55 -0300

    fix

--- menu/js/app.js
+++ menu/js/app.js
@@ -10,12 +10,18 @@ cardapio.eventos = {
 }
 cardapio.metodos = {
     //obtem a lista de itens do carpadio
-    obterItensCardapio :(categoria = 'burgers' ) => {
+    obterItensCardapio :(categoria = 'burgers' , vermais = false) => {
 
         var filtro = MENU[categoria];
         console.log(filtro);
 
-        $("#itensCardapio").html('')
+        if (!vermais){
+            $("#itensCardapio").html('');
+            $("#btnVerMais").removeClass('hiden');
+
+        }
+
+       
 
         $.each(filtro,(i,e) => {
 
@@ -23,13 +29,33 @@ cardapio.metodos = {
             .replace(/\${name}/g,e.name) 
             .replace(/\${price}/g, e.price.toFixed(2).replace('.' , ','))
 
-            $("#itensCardapio").append(temp)
+            //botão ver mais foi clicado (12 itens)
+            if(vermais && i >= 8 && i < 12){
+                $("#itensCardapio").append(temp)
+            }
+
+            //paginação inicial (8 itens)
+            if(!vermais && i < 8){
+                $("#itensCardapio").append(temp)
+            }
+           
+
         })
+
         // remove o ative
         $(".container-menu a").removeClass('active');
 
     // seta o menu para activo
         $("#menu-" + categoria).addClass('active')
+    },
+
+    //clique no botão de ver mais
+    verMais: () =>{
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; // [menu] [burgers]
+
+        cardapio.metodos.obterItensCardapio(ativo,true)
+
+        $("#btnVerMais").addClass('hidden');
     },
 }
 cardapio.templates = {
